4803a501b150b5afc6b6f8e85f4df03999f65d18
joliva <joliva63@gmail.com>
2013-06-25 13:01:46 +0000

    Second sample.

--- week2/samples/hello2.js
+++ week2/samples/hello2.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
-console.log("Hello World!")
 
+var fs = require('fs');
+var outfile = 'hello.txt';
+var out = 'Modify this script to write out something different';
+
+fs.writeFileSync(outfile, out);

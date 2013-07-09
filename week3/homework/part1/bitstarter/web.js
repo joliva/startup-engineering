c8d73575deb7acb310a3f2ce92f993d5b149a64f
joliva <joliva63@gmail.com>
2013-07-09 14:18:13 +0000

    Week 3 homework part 1.

---
+++ week3/homework/part1/bitstarter/web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	var buf = fs.readFileSync('index.html');
+	response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
